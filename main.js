df404c702be40abaab24db6dcc8a5a99efaa4746
Gugguuuuu <148577471+Gugguuuuu@users.noreply.github.com>
2023-12-20 13:33:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+let historia = document.getElementById('historia')
+function comecarHistoria(){
+    let nome = window.prompt("Qual seu primeiro nome?")
+    let sobrenome = window.prompt(" Qual é seu sobrenome?")
+    let qualesuaidade = window.prompt("Qual é sua idade?(responder em numeros)")
+    let ondenasceu = window.prompt(" Em qual cidade você nasceu?")
+    let comquevocetrabalha = window.prompt("Com oque você trabalha?")
+    let oquegostadefazer = window.prompt("Oque você gosta de fazer?")
+    let oquevoceebom = window.prompt("Você é bom no que?")
+    let historiaconcluida = document.getElementById("historia").innerHTML = " Olá meu nome é "  + nome + " " + sobrenome + " " + "e tenho" + " " + qualesuaidade + " " + "anos de idade, nasci na cidade de " + ondenasceu + ", " + "trabalho com " + comquevocetrabalha + "," + " gosto de " + oquegostadefazer + " e " + "sou bom em " + oquevoceebom ;
+    let botaosumir = document.getElementById('comecarHistoria').style.visibility = "hidden"
+}
+nome.style.fontSize = "70px"
+
+// function start(){
+//    let primeiroNome =  window.prompt("Digite seu  primeiro nome")
+//    let sobrenome=  window.prompt("Digite seu sobrenomenome")
+//    resultado = window.alert(primeiroNome + " " + sobrenome)
+
+// }
+// // çqefkjvn wji nilrw nbvkjw jsn liunlGCECBDS,C V \BC L\NC Kebcl HELÇHEÇO   curi4vlQJCKRGYREFVVJRÇLW\v]PCVLXL,CVejdo3´wdL`09e3Xl
+// lvujhb s\mn mbcl n\ecnçkmz/.Z
